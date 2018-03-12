3427903cca83ee4f4cb7e020164af8069f94a448
Teagan Nouska <tnouska@live.com>
2018-03-12 13:22:23 -0500

    Generate button now adds blank DIV element to DOM

--- client.js
+++ client.js
@@ -3,3 +3,12 @@ $(document).ready(readyNow);
 function readyNow() {
   addClickHandlers();
 }
+
+function addClickHandlers() {
+  $('#genButton').on('click', createDiv);
+}
+
+function createDiv() {
+  let newDiv = '<div class="divItem"></div>';
+  $('#holdDiv').append(newDiv);
+}
